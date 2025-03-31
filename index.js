5a0f20a2639b6567120ad174864b9ec345488cae
sirac007006 <markonis007006@gmail.com>
2025-03-31 11:49:30 +0200

    Dodate

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ app.use(express.static("public"));
 app.get("/", async(req, res) =>{
     res.render("index.ejs");
 })
-
+app.get("/kontakt", async(req,res) =>{
+    res.render("kontakt.ejs");
+})
+app.get("/prijava", async(req,res) =>{
+    res.render("prijava.ejs");
+})
+app.get("/registracija", async(req,res) =>{
+    res.render("registracija.ejs");
+})
+app.get("/zaboravljenalozinka", async(req,res) =>{
+    res.render("email.ejs");
+})
+app.get("/korpa", async(req,res) =>{
+    res.render("korpa.ejs");
+});
+app.get("/kupovina", async(req,res) =>{
+    res.render("kupovina.ejs");
+})
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
   });
